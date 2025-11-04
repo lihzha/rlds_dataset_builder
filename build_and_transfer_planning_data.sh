3c57794494c4476621512a814493cfe11abc09ff
lihzha <lihanzha20@gmail.com>
2025-11-03 22:01:10 -0500

    update script

--- build_and_transfer_planning_data.sh
+++ build_and_transfer_planning_data.sh
@@ -9,10 +9,17 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate rlds
 cd ~/prbench_dir/tidybot_planner/data/rlds_dataset_builder/planning_dataset
-tfds build --overwrite
 
-#2. Rename existing dataset on GCS. Read new dataset name from env variable
-gsutil mv gs://pi0-cot/OXE/planning_dataset gs://pi0-cot/OXE/${OLD_DATASET_NAME}
+# Run TFDS build; only continue if successful
+if tfds build --overwrite; then
+    echo "✅ Step 1 completed successfully, proceeding to upload."
 
-# 3. Transfer new dataset to GCS
-gsutil -m cp -r ~/tensorflow_datasets/planning_dataset gs://pi0-cot/OXE/planning_dataset
+    # 2. Rename existing dataset on GCS. Read new dataset name from env variable
+    gsutil mv gs://pi0-cot/OXE/planning_dataset gs://pi0-cot/OXE/${OLD_DATASET_NAME}
+
+    # 3. Transfer new dataset to GCS
+    gsutil -m cp -r ~/tensorflow_datasets/planning_dataset gs://pi0-cot/OXE/planning_dataset
+else
+    echo "❌ TFDS build failed. Aborting remaining steps."
+    exit 1
+fi
